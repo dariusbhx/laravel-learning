6e28a6ea309534d61ba088085fbce4db5d30ad77
Darius Brown <dariusbrown1999@hotmail.com>
2023-05-31 01:59:10 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $posts = Post::all();
+    
+    return view('posts', [
+        'posts' => $posts
+    ]);
+});
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})->where('post', '[A-z_\-]+');
