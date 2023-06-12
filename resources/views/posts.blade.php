73d0729d6f144b66dbb58ac358f68f2a762a8e96
Darius Brown <dariusbrown1999@hotmail.com>
2023-06-12 18:57:42 +0100

    started styling

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,17 @@
 <x-layout>
-    @foreach($posts as $post)
-        <article>
-            <a href="/posts/{{$post->slug}}">
-                <h1>
-                    <?= $post->title; ?>
-                </h1>
-            </a>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        
+        <x-post-card-lg />        
 
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name;}}</a>
-            </p>
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
 
-            <p>
-                <a href="categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
-            </p>
-            <div>{{ $post->excerpt }}</div>
-        </article>
-    @endforeach
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
 </x-layout>
